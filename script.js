f40327391abecc4aebc1e3d4890ab1661ff26d21
Didociciif <discovervs23@gmail.com>
2024-10-19 19:10:46 +0100

    Atualizações para integrar a API do JustWatch

--- script.js
+++ script.js
@@ -24,6 +24,12 @@ async function getUpcomingMovies(platform) {
   displayMovies(data.results);
 }
 
+async function getJustWatchUpcoming() {
+  const response = await fetch('https://apis.justwatch.com/content/titles/en_US/popular?body={"content_type":"movie","page_size":10,"provider_ids":[8,49,119,337],"sort_by":"release_date"}');
+  const data = await response.json();
+  displayJustWatchMovies(data.items);
+}
+
 function displayMovies(movies) {
   const moviesContainer = document.getElementById('movies');
   moviesContainer.innerHTML = '';
@@ -34,6 +40,24 @@ function displayMovies(movies) {
 
     movieItem.innerHTML = `
       <img src="https://image.tmdb.org/t/p/w200${movie.poster_path}" alt="${movie.title}">
+      <h3>${movie.title}</h3>
+      <p>Data de Lançamento: ${movie.release_date}</p>
+    `;
+
+    moviesContainer.appendChild(movieItem);
+  });
+}
+
+function displayJustWatchMovies(movies) {
+  const moviesContainer = document.getElementById('movies');
+  moviesContainer.innerHTML = '';
+
+  movies.forEach(movie => {
+    const movieItem = document.createElement('div');
+    movieItem.classList.add('movie-item');
+
+    movieItem.innerHTML = `
+      <img src="${movie.poster_path}" alt="${movie.title}">
       <h3>${movie.title}</h3>
       <p>Data de Lançamento: ${movie.release_date}</p>
     `;
